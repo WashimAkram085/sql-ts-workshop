7e94ac5e50e9c2102bd05040662d060d1b461a72
Washim Akram <Washim.Akram@godigit.com>
2024-08-08 11:27:25 +0530

    new product with name Realme added successfully

--- Build/order.js
+++ Build/order.js
@@ -1,2 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const product_1 = require("./product");
+// new product having Realme added successfully
+const addProductResponse = (0, product_1.addProduct)('Realme', 15000, '64GB', 10, 3.5);
+console.log(addProductResponse);
+//console.log(products);
--- src/order.ts
+++ src/order.ts
@@ -1,2 +1,8 @@
 import products, {addProduct} from "./product";
 
+// new product having Realme added successfully
+const addProductResponse = addProduct('Realme', 15000, '64GB', 10, 3.5);
+console.log(addProductResponse);
+
+//console.log(products);
+
